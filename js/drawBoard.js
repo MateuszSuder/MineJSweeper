3aeb5d37167bf597318ead70dacce62ec0cb1d98
Mateusz Suder <matisu999@gmail.com>
2020-07-16 16:48:23 +0200

    Added resizing function depending on resolution ratio

--- js/drawBoard.js
+++ js/drawBoard.js
@@ -2,6 +2,7 @@
 //This file contains html/css related functions, which basicly create the game
 let columns = 10;
 let rows = 10;
+let bombs = 90;
 function createBoard(width, height) {
     let table = document.createElement("table");
     for (let i = 1; i <= height; i++) {
@@ -10,7 +11,7 @@ function createBoard(width, height) {
             let td = document.createElement("td");
             let square = document.createElement("div");
             square.classList.add("square");
-            square.setAttribute("id", j + "," + i);
+            square.setAttribute("id", i + "," + j);
             td.appendChild(square);
             tr.appendChild(td);
         }
@@ -18,9 +19,36 @@ function createBoard(width, height) {
     }
     let game = document.getElementById("game");
     game.appendChild(table);
+    fit(width, height);
+}
+$(window).resize(function () {
+    fit(columns, rows);
+});
+function fit(width, height) {
+    if ((width / height) <= (($(window).width()) / ($(window).height()))) { //If column/rows ratio is smaller or equal to window resoultion ratio
+        //We need to care more about height in that case
+        let square = document.getElementsByClassName("square");
+        let arr = Array.from(square);
+        let size = 90 / height;
+        for (let index in arr) {
+            arr[index].style.width = size + "vh";
+            arr[index].style.height = size + "vh";
+            arr[index].style.borderWidth = "0.5vh";
+        }
+    }
+    else {
+        //We need to care more about width in that case
+        let square = document.getElementsByClassName("square");
+        let arr = Array.from(square);
+        let size = 90 / width;
+        for (let index in arr) {
+            arr[index].style.width = size + "vw";
+            arr[index].style.height = size + "vw";
+            arr[index].style.borderWidth = "0.5vw";
+        }
+    }
 }
 $(window).contextmenu(function (e) {
     e.preventDefault();
 });
 createBoard(columns, rows);
-//resize(columns, rows);
--- js/drawBoard.ts
+++ js/drawBoard.ts
@@ -2,6 +2,7 @@
 
 let columns = 10;
 let rows = 10;
+let bombs = 90;
 
 function createBoard(width: number, height: number):void{
     let table = document.createElement("table");
@@ -11,7 +12,7 @@ function createBoard(width: number, height: number):void{
             let td = document.createElement("td");
             let square:HTMLElement = document.createElement("div");
             square.classList.add("square");
-            square.setAttribute("id", j + "," +i);
+            square.setAttribute("id", i + "," +j);
             td.appendChild(square);
             tr.appendChild(td);
         }
@@ -19,11 +20,40 @@ function createBoard(width: number, height: number):void{
     }
     let game = document.getElementById("game");
     game!.appendChild(table);
+    fit(width, height);
+    
 }
+$(window).resize(function(){
+    fit(columns, rows);
+})
 
+
+function fit(width: number, height: number):void{
+    if((width/height) <= (($(window).width()!)/($(window).height()!))){ //If column/rows ratio is smaller or equal to window resoultion ratio
+        //We need to care more about height in that case
+        let square = document.getElementsByClassName("square") as HTMLCollectionOf<HTMLElement>;
+        let arr = Array.from(square);
+        let size = 90/height;
+        for(let index in arr){
+            arr[index].style.width = size + "vh";
+            arr[index].style.height = size + "vh";
+            arr[index].style.borderWidth = "0.5vh";
+        }
+    }
+    else{
+        //We need to care more about width in that case
+        let square = document.getElementsByClassName("square") as HTMLCollectionOf<HTMLElement>;
+        let arr = Array.from(square);
+        let size = 90/width;
+        for(let index in arr){
+            arr[index].style.width = size + "vw";
+            arr[index].style.height = size + "vw";
+            arr[index].style.borderWidth = "0.5vw";
+        }
+    }
+}
 $(window).contextmenu(function(e){
     e.preventDefault();
 })
 
 createBoard(columns, rows);
-//resize(columns, rows);
